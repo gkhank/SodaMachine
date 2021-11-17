610ca02ebc116defcd169d73d961b3bd2c770a14
Gökhan Kutucularoglu <gkhanmk@hotmail.com>
2021-11-17 20:57:23 +0100

    Comments

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -16,3 +16,11 @@ namespace ConsoleApplication1
         }
     }
 }
+
+
+
+//-	Resolve dependencies, do necessary changes so that it is easy to add
+//- New vending functions
+//- New soda types
+//-	Add unit tests testing important parts of the code
+//-	Make the code “pretty”, so that it’s easy for other developers to take it over, and maintain it in the future
